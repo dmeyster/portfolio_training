bb5175a9a8117957b72e1bb34065f15447aa8daa
John Amirov <birzhan.amirov@cloudkitchens.com>
2023-12-30 14:22:24 -0800

    String to Enum conversion using valueOf.

--- src/main/java/spring/example/java/portfolio_training/converters/StringToEnumConvertor.java
+++ src/main/java/spring/example/java/portfolio_training/converters/StringToEnumConvertor.java
@@ -6,13 +6,9 @@ import spring.example.java.portfolio_training.beans.Gender;
 public class StringToEnumConvertor implements Converter<String, Gender> {
     @Override
     public Gender convert(String s) {
-        if(s.equals("Male")){
-            return Gender.MALE;
-        }
-        else if(s.equals("Female")){
-            return Gender.FEMALE;
-        }
-        else{
+        try {
+            return Gender.valueOf(s.toUpperCase());
+        } catch (Exception e) {
             return Gender.OTHER;
         }
     }
